185a20d71ed60a68bb713ead1e7142f934353995
kisung-sim <kisung.sim@samsung.com>
2026-02-26 15:14:28 +0900

    add freeze.py

---
+++ freeze.py
@@ -0,0 +1,8 @@
+from flask_frozen import Freezer
+from app import app  # app.py에서 생성한 Flask 객체(app)를 가져옵니다.
+
+freezer = Freezer(app)
+
+if __name__ == '__main__':
+    # Flask 앱을 정적 파일(HTML)로 변환하여 'build' 폴더에 저장합니다.
+    freezer.freeze()
